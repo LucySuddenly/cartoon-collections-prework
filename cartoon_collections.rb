b00b89fb869dd761f7206b36f84c9582b86fede4
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-15 22:03:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+def roll_call_dwarves(names)
+  names.each_with_index do |dwarf, index|
+    puts "#{index + 1} #{dwarf}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect do |element| 
+    element = element.capitalize
+    element = element + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
